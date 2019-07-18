1c2bd970004e15de40057ed99bda70cab19ee0ac
Sirley Castillo <sirley..castillo89@gmail.com>
2019-07-19 00:26:38 +0200

    guardar el trabajo nsobre web

--- pendientes/views.py
+++ pendientes/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from pendientes.models import Tarea
+from random import randint
 # Create your views here.
 
 def index(request):
-    saludo = "Hola, Mundo! Esta es la raiz /"
-    return HttpResponse(saludo) #retornamos el saludo
+    listita= Tarea.objects.all()
+    persona = {
+    "nombre": "Sirley",
+     "edad": 28, 
+     "hobbies":["aprender cosas nuevas","leer","crear paginas web"],
+     "lista_tareas": listita,
+    }
+    return render(request,"inicio.html", persona) #retornamos el saludo
+
+def tarea(request):
+    return HttpResponse("hola soy la tarea")
+
+def music(request):
+    return HttpResponse("queeee vesss que ves cuando me veees")
